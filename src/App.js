b82e0ea53b1b121c33f626e66f369528fb4c9ae8
shaadhu11 <mhdshahad11@gmail.com>
2024-08-19 16:19:50 +0530

    intial login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import './components/login'
 
 function App() {
   return (
-    <h1>login page</h1>
+    <>
+      <h1>login page</h1>
+      <Login/>
+    </>
+    
       );
 }
 
