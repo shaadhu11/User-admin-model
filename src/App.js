03c1ea5c1f9c941e8fbf5397c7bc1f3bbed9b3ea
shaadhu11 <mhdshahad11@gmail.com>
2024-08-20 12:52:09 +0530

    signIn-signup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import Reactfr from 'react';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import './App.css';
 import SignUp from './components/loginSignup/SignUp.js';
 import SignIn from './components/loginSignIn/SignIn.jsx';
@@ -7,9 +8,14 @@ import SignIn from './components/loginSignIn/SignIn.jsx';
 function App() {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element ={<SignIn/>}/>
+          <Route path='/SignUp' element ={<SignUp/>}/>
+        </Routes>
+      </Router>
     
-      <SignIn/>
+      
     </>
     
       );
